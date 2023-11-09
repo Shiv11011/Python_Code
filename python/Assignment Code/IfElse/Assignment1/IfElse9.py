207cdfc9f0bd1ca1a57bea766fc75ab333ca2f07
Shivam Bhansali <113971880+Shiv11011@users.noreply.github.com>
2023-11-09 19:34:23 +0530

    Update IfElse9.py

--- python/Assignment Code/IfElse/Assignment1/IfElse9.py
+++ python/Assignment Code/IfElse/Assignment1/IfElse9.py
@@ -1,3 +1,4 @@
+#Program to check given character is vowel consonant or a special character
 char = input("Enter the character: ")
 if len(char) == 1 and (char=='a' or char=='e' or char=='i' or char=='o' or char=='u' or char=='A' or char=='E' or char=='I' or char=='O' or char=='U'):
 	print("Vowel")
