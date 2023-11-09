735c826ec9a9c51a305102cedf9a381ea8b8202b
Shivam Bhansali <113971880+Shiv11011@users.noreply.github.com>
2023-11-09 15:35:37 +0530

    Update ForLoop10.py

--- python/Assignment Code/forLoop/Assignment2/ForLoop10.py
+++ python/Assignment Code/forLoop/Assignment2/ForLoop10.py
@@ -4,5 +4,9 @@
 	6 5 4
        10 9 8 7
 '''
-for i in range(5):
-	for j in range(l):
+for i in range(1, 4):
+    num = i
+    for j in range(i, 0, -1):
+        print(num, end=" ")
+        num += 1
+    print()
